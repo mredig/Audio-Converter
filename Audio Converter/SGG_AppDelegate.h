c5d2829d6a3619011530688458018b242920d089
Michael <Use Website Contact Please>
2014-09-16 20:34:31 -0500

    Initial Commit

---
+++ Audio Converter/SGG_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGG_AppDelegate.h
+//  Audio Converter
+//
+//  Created by Michael Redig on 9/16/14.
+//  Copyright (c) 2014 Secret Game Group LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SGG_AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Audio Converter/SGG_AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SGG_AppDelegate.m
+//  Audio Converter
+//
+//  Created by Michael Redig on 9/16/14.
+//  Copyright (c) 2014 Secret Game Group LLC. All rights reserved.
+//
+
+#import "SGG_AppDelegate.h"
+
+@implementation SGG_AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
